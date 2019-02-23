750ace4589bf880815edbaa015aceae1d36242a6
katherine-a <katherine_a.su@yahoo.com>
2019-02-23 13:56:06 -0800

    example

---
+++ src/components/Game/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.css';
+
+class Game extends React.component {
+render() {
+    return(
+<div classname="Game"></div>
+    );
+}
+}
+
+export default Game;
